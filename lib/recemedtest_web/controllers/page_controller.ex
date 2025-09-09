e87f774831943773d186e99077a1c02e8ab51167
Marco Salinas <salinas.marco.93@gmail.com>
2025-09-09 17:14:07 -0300

    feat: redirect to user log in without session

--- lib/recemedtest_web/controllers/page_controller.ex
+++ lib/recemedtest_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule RecemedtestWeb.PageController do
   use RecemedtestWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    if @current_scope do
+      redirect(conn, to: ~p"/users/log-in")
+    else
+      redirect(conn, to: ~p"/admin/practitioners")
+    end
   end
 end
